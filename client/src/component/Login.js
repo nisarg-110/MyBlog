9f57717823a29b9ac018ffb6c7c703bef8424787
Nisarg Patel <109233327+nisarg-110@users.noreply.github.com>
2024-06-18 21:35:13 +0530

    login-register page

--- client/src/component/Login.js
+++ client/src/component/Login.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Header from './Header'
 
 function Login() {
   return (
-    <div>
-      Login Page
+    <div className='loginContainer'>
+      <form action=''>
+        <h1>Login</h1>
+        <input type='text' placeholder='Username' />
+        <input type='password' placeholder='Password' />
+        <button>Login</button>
+      </form>
     </div>
   )
 }
--- client/src/component/Register.js
+++ client/src/component/Register.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 function Register() {
   return (
-    <div>
-      Register Page
+    <div className='registerContainer'>
+      <form className='register'>
+        <h1>Register</h1>
+        <input type='text' placeholder='Username' />
+        <input type='password' placeholder='Password' />
+        <button>Register</button>
+      </form>
     </div>
   )
 }
